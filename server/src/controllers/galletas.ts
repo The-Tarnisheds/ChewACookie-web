6884c598bba1fd9d31c342f9b9785421628ea9a1
Showtime7 <edrian.gutierrez@gmail.com>
2025-06-20 18:22:42 -0400

    Se actualiza logica de galletas

--- server/src/controllers/galletas.ts
+++ server/src/controllers/galletas.ts
@@ -3,29 +3,23 @@ import { Request, Response } from "express";
 import HttpStatusCode from "../utils/http-status-code";
 import { errorHandler, CustomError } from "../utils/error_handler";
 
-
-const getProducts = async (req: Request, res: Response): Promise<any> =>  {
-
-    try {
-        const products = await Producto.findAll()
-        if(!products){
-            return res.status(HttpStatusCode.NOT_FOUND).json({
-                message: 'No se encontro el producto.',
-                success: false,
-            })
-        }
-
-        res.status(HttpStatusCode.OK).json({
-            message: 'Fetched products successfully.',
-            results: products,
-        })
-
-            
-    } catch (error) {
-        errorHandler(error as CustomError | undefined, req, res)
+const getProducts = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const products = await Producto.findAll();
+    if (!products) {
+      return res.status(HttpStatusCode.NOT_FOUND).json({
+        message: "No se encontro el producto.",
+        success: false,
+      });
     }
-}
 
-export {
-    getProducts
-}
+    res.status(HttpStatusCode.OK).json({
+      message: "Fetched products successfully.",
+      results: products,
+    });
+  } catch (error) {
+    errorHandler(error as CustomError | undefined, req, res);
+  }
+};
+
+export { getProducts };
